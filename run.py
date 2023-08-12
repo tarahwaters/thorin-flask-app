20762ec747b51b0f7c23e59122318742cd2e7783
tarahwaters <thbwaters@outlook.com>
2023-08-12 15:09:09 +0000

    Replace print statements in contact route with flash function call

--- run.py
+++ run.py
@@ -35,8 +35,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
