8e1381246728a77df971ce716ce4ebd21db9e315
tarahwaters <thbwaters@outlook.com>
2023-08-12 15:18:51 +0000

    Add code to retrieve the flashed messages within the contact form

--- run.py
+++ run.py
@@ -35,7 +35,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
