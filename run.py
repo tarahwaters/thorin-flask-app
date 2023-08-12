8ca9d31a043b8dfe8af3d72620703f511131aad5
tarahwaters <thbwaters@outlook.com>
2023-08-12 12:33:17 +0000

    Add a print test for contact method route that returns name and email data from the form

--- run.py
+++ run.py
@@ -32,7 +32,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print("Hello! Is anybody there?")
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
