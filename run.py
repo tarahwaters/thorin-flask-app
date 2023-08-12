a6936ff99e4a61b6b1b7d42444735dec5f39a044
tarahwaters <thbwaters@outlook.com>
2023-08-12 12:26:29 +0000

    Add GET and POST methods to contact route in python file

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     return render_template("contact.html", page_title="Contact")
 
